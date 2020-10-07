a232046a9ae1e3040e7fba8f37c194ea5d50be73
Nikita <69010558+Kapibarra@users.noreply.github.com>
2020-10-07 21:00:45 +0300

    redux

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -4,6 +4,7 @@ import { createTable } from '@/components/table/table.template'
 import { resizeHandler } from '@/components/table/table.resize'
 import { isCell, matrix, shouldResize, nextSelector } from '@/components/table/table.functions'
 import { TableSelection } from '@/components/table/TableSelection'
+import * as actions from "@/redux/actions"
 
 
 export class Table extends ExcelComponent {
@@ -18,7 +19,7 @@ export class Table extends ExcelComponent {
     }
 
     toHTML() {
-        return createTable(20)
+        return createTable(20, this.store.getState())
     }
 
     prepare() {
@@ -37,6 +38,10 @@ export class Table extends ExcelComponent {
         this.$on('formula:done', () => {
             this.selection.current.focus()
         })
+
+        this.$subscribe((state) => {
+            console.log('tableState', state)
+        })
     }
 
     selectCell($cell) {
@@ -44,9 +49,18 @@ export class Table extends ExcelComponent {
         this.$emit('table:select', $cell)
     }
 
+    async resizeTable(event) {
+        try {
+            const data = await resizeHandler(this.$root, event)
+            this.$dispatch(actions.tableResize(data))
+        } catch (e) {
+            console.warn('resize error', e.message)
+        }
+    }
+
     onMousedown(event) {
         if (shouldResize(event)) {
-            resizeHandler(this.$root, event)
+            this.resizeTable(event)
         } else if (isCell(event)) {
             const $target = $(event.target)
             if (event.shiftKey) {
@@ -54,7 +68,7 @@ export class Table extends ExcelComponent {
                     .map((id) => this.$root.find(`[data-id="${id}"]`))
                 this.selection.selectGroup($cells)
             } else {
-                this.selection.select($target)
+                this.selectCell($target)
             }
         }
     }
