4b7ddc58c121900e440c122f83cf3ac98e0e8183
Nikita <69010558+Kapibarra@users.noreply.github.com>
2020-10-07 13:34:46 +0300

    123

--- src/core/dom.js
+++ src/core/dom.js
@@ -11,6 +11,18 @@ class Dom {
         }
         return this.$el.outerHTML.trim()
     }
+
+    text(text) {
+        if (typeof text === 'string') {
+            this.$el.textContent = text
+            return this
+        }
+        if (this.$el.tagName.toLowerCase() === 'input') {
+            return this.value.$el.value.trim()
+        }
+        return this.$el.textContent.trim()
+    }
+
     clear() {
         this.html("")
         return this
@@ -24,6 +36,10 @@ class Dom {
         this.$el.removeEventListener(eventType, callback)
     }
 
+
+    find(selector) {
+        return $(this.$el.querySelector(selector))
+    }
     append(node) {
         if (node instanceof Dom) {
             node = node.$el
@@ -57,9 +73,28 @@ class Dom {
                 this.$el.style[key] = styles[key]
             })
     }
-}
+    id(parse) {
+        if (parse) {
+            const parsed = this.id().split(':')
+            return {
+                row: +parsed[0],
+                col: +parsed[1],
+            }
+        }
+        return this.data.id
+    }
+    focus() {
+        this.$el.focus()
+        return this
+    }
 
-$('div').html('<h1>TEST</h1>').clear()
+    addClass(className) {
+        this.$el.classList.add(className)
+    }
+    removeClass(className) {
+        this.$el.classList.remove(className)
+    }
+}
 
 export function $(selector) {
     return new Dom(selector)
