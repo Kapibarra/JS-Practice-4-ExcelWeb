4fdccedae6fa196aeb59c4068105b089468802de
Nikita <69010558+Kapibarra@users.noreply.github.com>
2020-10-14 17:17:35 +0300

    finished

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,6 +2,8 @@ import { ExcelComponent } from "../../core/ExcelComponent";
 import { $ } from "../../core/dom"
 import { changeTitle } from "../../redux/actions";
 import { defaultTitle } from "../../constants";
+import { ActiveRoute } from "../../core/routes/ActiveRoute";
+import { debounce } from '@core/utils'
 
 export class Header extends ExcelComponent {
     static className = 'excel__header'
@@ -9,24 +11,41 @@ export class Header extends ExcelComponent {
     constructor($root, options) {
         super($root, {
             name: 'Header',
-            listeners: ['input'],
+            listeners: ['input', 'click'],
             ...options,
         })
     }
+
+    prepare() {
+        this.onInput = debounce(this.onInput, 300)
+    }
+
     toHTML() {
         const title = this.store.getState().title || defaultTitle
         return `
         <input type="text" class="input" value="${title}" />
           <div>
-            <div class="button">
-              <i class="material-icons">delete</i>
+            <div class="button" data-button="remove">
+              <i class="material-icons" data-button="remove">delete</i>
             </div>
 
-            <div class="button">
-              <i class="material-icons">exit_to_app</i>
+            <div class="button" data-button="exit">
+              <i class="material-icons" data-button="exit">exit_to_app</i>
             </div>
           </div>
         `
+    }
+    onClick(event) {
+        const $target = $(event.target)
+        if ($target.data.button === 'remove') {
+            const decigion = confirm('Вы действительно хотите удалить эту таблицу?')
+            if (decigion) {
+                localStorage.removeItem('excel:' + ActiveRoute.param)
+                ActiveRoute.navigate('')
+            }
+        } else if ($target.data.button === 'exit') {
+            ActiveRoute.navigate('')
+        }
     }
     onInput(event) {
         const $target = $(event.target)
